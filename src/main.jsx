bfd50db6c97e2048bbe434d9d0dd29a48aa09aee
Forrest Shen <forrestshen@protonmail.com>
2026-03-21 21:34:49 -0400

    Refactor hex-cosmic-grid implementation for improved performance and readability

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import HexCosmicGrid from '../hex-cosmic-grid.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HexCosmicGrid />
+  </React.StrictMode>,
+)
